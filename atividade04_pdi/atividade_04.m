b4d34221f11c04c4969e91e67d870121c08ffbdd
limaalan <alanlimamarques6@gmail.com>
2024-04-21 18:25:57 -0300

    Finalizado atividade 4

--- atividade04_pdi/atividade_04.m
+++ atividade04_pdi/atividade_04.m
@@ -1,4 +1,6 @@
-%% slide 42 
+%Atividade 04 de Processamento de imagens , entrega 15/04/2024
+%Alunos : Alan Lima Marques, Thiago Jacomo, Guilherme Inoue
+
 pkg load image ;
 
 %Carregando imagens
@@ -23,26 +25,48 @@ for i=1:tam_filtro
   endfor
 endfor
 
-
 %Aplicando o filtro na imagem
 img_saida = filter2(filtro,img) ;
 
 %Exibindo e salvando a imagem
 imshow(img_saida);
-imwrite(img_saida,"imgsaida.jpg")
-%Essa imagem ficou com uma borda preta por conta de estratégia escolhida para lidar
-%com o problema dos pixels da borda. A função filter2 adota como abordagem padrão
-%preencher os pixels de fora da imagem com o valor de intesidade 0 , o que escurece o 
-%pixel resultante quando se faz cálculos próximos a borda.
+imwrite(img_saida,"img_saida_filtro.jpg")
+
+% Essa imagem ficou com uma borda preta por conta de estratégia escolhida para lidar
+% com o problema dos pixels da borda. A função filter2 adota como abordagem padrão
+% preencher os pixels de fora da imagem com o valor de intesidade 0 , o que escurece o 
+% pixel resultante quando se faz cálculos próximos a borda. 
 
 %Criando um padding na imagem, aplicando o filtro, exibindo e salvando
-img = padarray( img, [tam_filtro-1 , tam_filtro-1] , 'replicate');
+img = padarray( img, [(tam_filtro-1)/2 ,( tam_filtro-1)/2] , 'replicate');
 img_saida = filter2(filtro,img,'valid');
 figure, imshow(img_saida);
-imwrite(img_saida,"imgsaida2.jpg")
+imwrite(img_saida,"img_saida_filtro_padding.jpg")
 
-%passo 6....
+%Montando os filtros de sobel
+filtro_sobel_1 = [-1 -2 -1 ; 0 0 0 ; 1 2 1]
+filtro_sobel_2 = [-1 0 1 ; -2 0 2 ; -1 0 1]
+img_sobel_1 = padarray( img_original, [1, 1] , 'replicate');
+img_sobel_2 = padarray( img_original, [1, 1] , 'replicate');
+img_saida_sobel_1=filter2(filtro_sobel_1,img_sobel_1,'valid') ;
+img_saida_sobel_2=filter2(filtro_sobel_2,img_sobel_2,'valid') ;
 
+%Aplicando filtros
+figure, imshow(img_saida_sobel_1);
+figure, imshow(img_saida_sobel_2);
+
+%Calculando a magnitude do gradiente 
+img_saida_sobel = zeros (width );
+img_saida_sobel = im2double (img_saida_sobel);
+for i=1:width
+  for j=1:height
+    img_saida_sobel(i,j) =abs(img_saida_sobel_1(i,j)) + abs(img_saida_sobel_2(i,j)) ;
+  endfor
+endfor
+
+%Salvando e exibindo
+figure, imshow(img_saida_sobel);
+imwrite(img_saida_sobel,"img_saida_filtro_sobel.jpg")
 
 
 
