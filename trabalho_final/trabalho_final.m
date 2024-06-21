896442f6cde76de36b50c753f82d72a8ab535d4f
limaalan <alanlimamarques6@gmail.com>
2024-06-20 23:07:23 -0300

    trabalho final

--- trabalho_final/trabalho_final.m
+++ trabalho_final/trabalho_final.m
@@ -1,26 +1,26 @@
-pkg load image;
+%Trabalho final da disciplina de Processamento de imagens
+% Alunos : Alan Lima Marques, Thiago Roberto Jacomo , Guilherme Inoue
 
-% Passo 0: Carregar e exibir a imagem original em RGB
+% Carregar e exibir a imagem original em RGB
+pkg load image
 img_original = imread('Imagem.jpg');
 figure,imshow(img_original);
 title('Imagem Original RGB');
 
+% Transformando em double e calculando dimensões da imagem
 img_original=im2double(img_original);
 [x,y,z] = size(img_original)
 
-%Transformando modelo de cor da imagem para HSV , e separando canal de brilho da imagem%
+%Transformando modelo de cor da imagem para HSV , e separando canal de brilho da imagem
 imagem_hsv = rgb2hsv(img_original);
 imagem_hsv = im2double(imagem_hsv);
 imagem_value = imagem_hsv(1:x,1:y,3);
 figure, imshow(imagem_value);
 title("Canal 'Value' da imagem em HSV ")
 
-%Definindo parâmetros
+% Aplicando a FFT e centralizando
 P = x * 2;
 Q = y * 2;
-
-% Aplicando a FFT e centralizando , as dimensões da transformada P e Q
-% são o dobro das dimensões originais da imagem .
 transformada = fft2(imagem_value, P, Q);
 transformada = fftshift(transformada);
 
@@ -33,8 +33,7 @@ imwrite(imagem_fft, "espectro.png");
 % carregando o filtro notch, criado manualmente em um programa de edição de imagens
 filtro_notch = imread("filtro.png");
 figure, imshow(double(filtro_notch));
-title("Filtro notch ");
-
+title("Filtro notch");
 filtro_notch = im2double(filtro_notch);
 
 % aplicando o filtro, descentralizando e extraindo a parte real
@@ -43,22 +42,40 @@ resultado_aplicacao_filtro = ifftshift(resultado_aplicacao_filtro);
 resultado_aplicacao_filtro = ifft2(resultado_aplicacao_filtro);
 resultado_aplicacao_filtro = real(resultado_aplicacao_filtro);
 
-% convertendo para uint8 e visualizando resultado.
-%resultado = im2uint8(resultado_aplicacao_filtro);
+% extraindo a parte real do resultado
 resultado = im2double(resultado_aplicacao_filtro);
 resultado = resultado(1:x,1:y);
 
-%Exibindo resultado da filtragem no canal 'Value'
-%figure, imshow(resultado);
-%title("Imagem filtrada");
-%imwrite(resultado, "resultado.png");
-
+% reconstituindo a imagem, devolvendo o resultado ao terceiro canal (V)
 imagem_hsv = imagem_hsv(1:x, 1:y ,1:2);
 imagem_hsv = cat ( 3, imagem_hsv , resultado);
 
 resultado_final = hsv2rgb(imagem_hsv);
 figure, imshow(resultado_final);
 title("Resultado final");
-imwrite(resultado_final, 'resultado_final.png')
+imwrite(resultado_final, "resultado_filtragem.png")
 
+%------------------- Correção de contraste -------------------------%
+
+% Calcular o histograma
+[counts, binLocations] = imhist(resultado_final);
+
+% Calcular o CDF (Cumulative Distribution Function)
+cdf = cumsum(counts) / numel(resultado_final);
+
+% Definir os limites inferior e superior da intensidade desejada
+low_in = 0.0; % dos pixels mais escuros
+high_in = 0.9; % dos pixels mais claros
+
+% Encontrar os níveis de intensidade correspondentes aos percentis desejados
+low_limit = find(cdf >= low_in, 1);
+high_limit = find(cdf >= high_in, 1);
+
+% muda a faixa do intervalo de intensidade
+contraste_ajustado = imadjust(resultado_final, [low_limit/255, high_limit/255], [0.1,1]);
+
+% Exibindo e salvando a imagem com correção de contraste
+figure, imshow(contraste_ajustado);
+title("Resultado final com contraste ajustado");
+imwrite(contraste_ajustado,"resultado_final.png");
 
